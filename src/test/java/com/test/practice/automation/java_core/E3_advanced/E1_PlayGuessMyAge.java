8563efdf14a942eb72147cc52e70114effba7aa1
TPD_Auto <nooruddin1504@aol.com>
2017-03-03 17:16:25 +0000

    Updated based on feedback from learners

--- src/test/java/com/test/practice/automation/java_core/E3_advanced/E1_PlayGuessMyAge.java
+++ src/test/java/com/test/practice/automation/java_core/E3_advanced/E1_PlayGuessMyAge.java
@@ -11,8 +11,8 @@ public class E1_PlayGuessMyAge {
          * @Todo Aim: Create a game where user will be asked to guess the age
          * 1. Program will randomly set an age : AGE
          * 2. Than ask the user to take a guess : GUESS
-         * 3. If the GUESS is > than AGE than print out "I am younger than : " + GUESS age
-         *    If the GUESS is < than AGE than print out "I am older than : " + GUESS age
+         * 3. If the GUESS is more than AGE than print out "I am younger than : " + GUESS age
+         *    If the GUESS is less than AGE than print out "I am older than : " + GUESS age
          * 4. If GUESS is the same as AGE than print "Well done you have guessed my age correctly "
          */
         System.out.println("\n---------------New Exercise---------------\n");
